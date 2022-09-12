2d710c99cbbc655ec8b5f7d6d34ea207bb19445a
gabibble <juliagieb@gmail.com>
2022-09-12 07:55:09 -0700

    fixed form

--- trip_list/site/routes.py
+++ trip_list/site/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from trip_list.forms import SubmitTrip
-# from trip_list.models import Trip, db
+from flask_login import current_user
+from trip_list.models import Trip, db
 # from trip_list.helpers import token_required
 
 
@@ -15,7 +16,7 @@ def home():
 def profile():
     form = SubmitTrip()
     try:
-        if request.method == "POST": #and form.validate_on_submit():
+        if request.method == "POST" and form.validate_on_submit():
             #before it works, create form.html, form.py and send bp to innit
             trip_name = form.trip_name.data
             city = form.city.data
@@ -27,13 +28,13 @@ def profile():
             trip_date = form.trip_length.data
             #I think we need token to add a trip? Can't figure out how to get it
             #user_token = current_user_token.token #this is how we added it when adding a trip via api
-            # user_token = current_user.token #this is how it shows up on profile page. How does 
+            user_token = current_user.token #this is how it shows up on profile page. How does 
 
-            print(trip_name, city, state, country, people, accommodation, trip_length, trip_date) #, user_token=user_token)
+            print(trip_name, city, state, country, people, accommodation, trip_length, trip_date, user_token)
             # instantiiating trip(comment this put until u create user class and DB first!)
-            # trip = Trip(trip_name, city, state, country, people, accommodation, trip_length, trip_date)
-            # db.session.add(trip)
-            # db.session.commit()
+            trip = Trip(trip_name, city, state, country, people, accommodation, trip_length, trip_date , user_token=user_token)
+            db.session.add(trip)
+            db.session.commit()
             #
             flash(f"Your trip has been added!", 'user-created')
             return redirect(url_for('site.profile'))
